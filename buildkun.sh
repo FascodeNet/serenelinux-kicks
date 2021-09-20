b5a9ac51105d0c5e156a185bfdaca9b454bf915b
kokkiemouse <kokkiemouse@fascode.net>
2021-09-20 10:41:03 +0900

    vup

--- buildkun.sh
+++ buildkun.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-ksflatten --config fedora-livecd-xfce-ja_JP.ks -o flat.ks --version F34
-livemedia-creator --ks flat.ks --no-virt --resultdir /var/lmc --project SereneLinux-Live --make-iso --volid SereneLinux-34 --iso-only --iso-name SereneLinux-Xfce-34-x86_64.iso --releasever 34 --macboot
+ksflatten --config fedora-livecd-xfce-ja_JP.ks -o flat.ks --version F35
+livemedia-creator --ks flat.ks --no-virt --resultdir /var/lmc --project SereneLinux-Live --make-iso --volid SereneLinux-35 --iso-only --iso-name SereneLinux-Xfce-35-x86_64.iso --releasever 35 --macboot
